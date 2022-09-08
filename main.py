e64dc5edae57e45dfde11b4f72cca0674fabdb8d
andres <andres.gomez29@udea.edu.co>
2022-09-08 14:16:56 -0500

    Segundopaso

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 
 from udea_reservation_automation.bot import Bot
+import time
 
 def main():
-    bot=Bot()
-    bot.search_on_youtube()
-    bot.close()
+    bot=Bot() #create a new bot
+    bot.search_on_youtube("Risas de bebe remix")#search  for videos on  youtube
+    time.sleep(20)
+    bot.close() #close the bot
 
 
 if __name__ == "__main__":
     main()
+
 
--- udea_reservation_automation/bot.py
+++ udea_reservation_automation/bot.py
@@ -7,14 +7,18 @@ from selenium.webdriver.support import expected_conditions as EC
 class Bot:
     def __init__(self):
 
-        self.driver = webdriver.Chrome("storage/chromedriver")
-        self.wait_driver=WebDriverWait(self.driver,timeout=20)
+        self.driver = webdriver.Chrome("storage/chromedriver") #start with chromedriver
+        self.wait_driver=WebDriverWait(self.driver,timeout=20) #wait for open the browser
 
-    def search_on_youtube(self):
+    def search_on_youtube(self,input):
 
-        self.driver.get("https://www.youtube.com")
-        self.wait_driver.until(EC.presence_of_all_elements_located(By.ID,"search"))
-        self.wait_driver.find_element_by_id("search").click()
+        self.driver.get("https://www.youtube.com") #open youtube
+        #self.wait_driver.until(EC.presence_of_all_elements_located(By.ID())) #
+        self.searchBox=self.driver.find_element(By.XPATH,"/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input") #searchbox input
+        self.searchBox.send_keys(input)
 
+        self.searchButton=self.driver.find_element(By.XPATH,"/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/button") #search button
+        self.searchButton.click()
+    
     def close(self):
         self.driver.close()
