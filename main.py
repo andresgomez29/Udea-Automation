230262267291a52bb4b03b8ab1e0a1e0bdb03e0f
andres <andres.gomez29@udea.edu.co>
2022-09-08 19:25:56 -0500

    FirstStepUdea

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 
 from udea_reservation_automation.bot import Bot
+from udea_reservation_automation.bot import Bot_Meet_Room
 import time
 
 def main():
+
+    bot=Bot_Meet_Room()
+    bot.enter_page()
+    time.sleep(5)
+    bot.Log_in("andres.gomez29","lepava27")
+    time.sleep(5)
+    bot.library_interfaz()
+    time.sleep(5)
+    bot.meet_rooms_interfaz()
+    time.sleep(5)
+    bot.room1()
+    time.sleep(5)
+    bot.ReserveRoom()
+    time.sleep(30)
+    bot.close()
+
+'''
+
     bot=Bot() #create a new bot
     bot.search_on_youtube("Risas de bebe remix")
     time.sleep(10)
     bot.go_to_video()#search  for videos on  youtube
     time.sleep(20)
     bot.close() #close the bot
+'''
 
 
+
+
+    
 if __name__ == "__main__":
     main()
 
--- udea_reservation_automation/bot.py
+++ udea_reservation_automation/bot.py
@@ -26,3 +26,58 @@ class Bot:
 
     def close(self):
         self.driver.close()
+
+class Bot_Meet_Room():
+    def __init__(self):
+        self.driver = webdriver.Chrome("storage/chromedriver")
+        self.wait_driver=WebDriverWait(self.driver,timeout=20)  
+          
+    def enter_page(self):
+        self.driver.get("https://biblioteca.udea.edu.co/turnosudea/#/")
+
+
+
+    def Log_in(self,username,password):
+
+        self.UsenameBox=self.driver.find_element(By.XPATH,"/html/body/main/div/div/div/div/div/div/div[1]/div/div/div/form/div[3]/input")
+        self.UsenameBox.send_keys(username)
+
+        self.passwordBox=self.driver.find_element(By.XPATH,"/html/body/main/div/div/div/div/div/div/div[1]/div/div/div/form/div[4]/input")
+        self.passwordBox.send_keys(password)
+
+        self.log_in_Button=self.driver.find_element(By.XPATH,"/html/body/main/div/div/div/div/div/div/div[1]/div/div/div/form/div[5]/div/button")
+        self.log_in_Button.click()
+
+    def library_interfaz(self):
+        self.libraryBox=self.driver.find_element(By.XPATH,"/html/body/main/div/div/div/div/div[2]/div[1]/div/div/a")
+        self.libraryBox.click()
+    
+    def meet_rooms_interfaz(self):
+        self.meet_roomsBox=self.driver.find_element(By.XPATH,"/html/body/main/div/div/div/div/div[4]/div[3]/div/div/a")
+        self.meet_roomsBox.click()
+        
+
+    def room1(self):
+        self.room1Box=self.driver.find_element(By.XPATH,"/html/body/main/div/div/div/div/div[5]/div[1]/div/a")
+        self.room1Box.click()
+
+
+
+    def ReserveRoom(self):
+        self.horariosBox=self.driver.find_element(By.XPATH,"/html/body/main/div/div/div/div/div[4]/div[2]/div[2]/div/div/div[4]")
+        self.horariosBox.click()
+
+        self.reserveButton=self.driver.find_element(By.XPATH,"/html/body/main/div/div/div/div/div[4]/div[2]/div[3]/div/button")
+        self.reserveButton.click()
+        
+
+
+
+
+
+    def close(self):
+        self.driver.close()
+
+
+        
+
